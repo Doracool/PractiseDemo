47b5771dc5b57d1a8a689217ca147a1b8a112d6d
Doracool <751379858@qq.com>
2015-11-16 08:48:13 +0800

    修改了火焰动画，Tomcat的初级动画效果

---
+++ Tom猫/Tom猫/Button.h
@@ -0,0 +1,13 @@
+//
+//  Button.h
+//  Tom猫
+//
+//  Created by qingyun on 15/11/15.
+//  Copyright (c) 2015年 河南青云信息技术有限公司. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Button : NSObject
+
+@end
---
+++ Tom猫/Tom猫/Button.m
@@ -0,0 +1,13 @@
+//
+//  Button.m
+//  Tom猫
+//
+//  Created by qingyun on 15/11/15.
+//  Copyright (c) 2015年 河南青云信息技术有限公司. All rights reserved.
+//
+
+#import "Button.h"
+
+@implementation Button
+
+@end
